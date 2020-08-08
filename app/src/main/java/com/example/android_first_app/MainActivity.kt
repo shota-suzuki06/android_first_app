f2627bcba5749bcde639494e26fa8e64d352d43b
shota suzuki <shota.suzuki@gmail.com>
2020-08-08 17:43:01 +0900

    dataBindingの使用.

--- app/src/main/java/com/example/android_first_app/MainActivity.kt
+++ app/src/main/java/com/example/android_first_app/MainActivity.kt
@@ -5,18 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import com.example.android_first_app.databinding.ActivityMainBinding
+import androidx.databinding.DataBindingUtil
 
 // intentからmessageを取り出すためのkey
 const val EXTRA_MESSAGE = "com.example.android_first_app"
+
+private lateinit var binding: ActivityMainBinding
 
 // Activityとlayout.xmlは対になっている
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // setContentView を 下記の記述に置き換える
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, binding.editText.text.toString())
+            }
+            startActivity(intent)
+        }
     }
 
+    // DataBindingを使用しない例
     fun sendMessage(view: View) {
         val editText = findViewById<EditText>(R.id.editText)
         val message = editText.text.toString()
